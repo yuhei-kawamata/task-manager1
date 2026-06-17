77df6c599e1a5d559ba3981aaf5d035fad0e9126
yuhei-kawamata <y.kawamata7416@gmail.com>
2026-06-18 06:43:44 +0900

    feat: モデル作成とリレーション定義 #2

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,7 +44,7 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    public function task(): HasMany
+    public function tasks(): HasMany
     {
         return $this->hasMany(Task::class);
     }
